e72a2b0d2fa05a51b5824b2a5434d6ea27b3eb4c
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-10-18 13:02:17 +0600

    console input output

--- 1.Input Output/2.JOptionPane/1.ShowMessage.java
+++ 1.Input Output/2.JOptionPane/1.ShowMessage.java
@@ -3,5 +3,6 @@ import javax.swing.JOptionPane;
 class Driver {
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "I am a Programmer");
+		JOptionPane.showMessageDialog(null, "I am doing programming since 2015");
 	}
 }
---
+++ 1.Input Output/2.JOptionPane/2.ShowMessage.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+class Driver {
+	public static void main(String[] args) {
+		String message = "I am a Programmer.\n" + 
+				 "I am doing programming since 2015.\n" +
+				 "I also love to play games.\n" +
+                                 "I began game programming at 2017.\n" +
+                                 "My favourite game industry is Ubisoft.\n" +
+                                 "I want to work there some day.\n"; 
+
+
+		JOptionPane.showMessageDialog(null, message);
+	}
+}
