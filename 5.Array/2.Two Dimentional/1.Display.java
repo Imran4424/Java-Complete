8a868e6be7c638c578419a34d2504577a777f5c5
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-10-20 23:54:52 +0600

    array

---
+++ 5.Array/2.Two Dimentional/1.Display.java
@@ -0,0 +1,16 @@
+
+class Driver {
+	public static void main(String[] args) {
+		int[][] arr = new int[][]{{10, 45, 3, 8}, {2, 42}, {3, 21, 44}};
+
+		// displaying 2D array
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+		System.out.println();
+	}
+}
