79e5dce023efd9094332c624035d43ae404333f0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-10-20 21:26:07 +0600

    array

---
+++ 5.Array/1.One Dimentional/1.Display.java
@@ -0,0 +1,14 @@
+
+class Driver {
+	public static void main(String[] args) {
+		int[] arr = new int[]{1, 7, 5, 2, 8, 12, 15};
+
+		System.out.println("Array size is - " + arr.size());
+
+		for (int i = 0; i < arr.size(); i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+		System.out.println();
+	}
+}
